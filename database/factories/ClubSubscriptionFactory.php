5815ef3c458dd6db6868229e8f2f260b43c82aa0
Inferno-Team <infernoacm@gmail.com>
2023-07-18 19:29:27 +0300

    udpates

--- database/factories/ClubSubscriptionFactory.php
+++ database/factories/ClubSubscriptionFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ClubSubscriptionFactory extends Factory
+class ClubSubScriptionFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Club;
 use App\Models\ClubSubScription;
 use App\Models\User;
 use App\Models\UserSubscription;
-use Database\Factories\ClubSubscriptionFactory;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -22,7 +21,7 @@ class UserSeeder extends Seeder
     {
         User::factory()->has(
             Club::factory()->count(1)->has(
-                ClubSubscriptionFactory::factory()->count(2)->has(
+                ClubSubScription::factory()->count(2)->has(
                     UserSubscription::factory()->has(User::factory()->count(1)
                         ->state(function (array $attributes, User $user) {
                             return ['type' => 'customer'];
