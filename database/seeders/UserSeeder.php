66ad324abc9d65c3d6cbf316d32085df64851e04
Inferno-Team <infernoacm@gmail.com>
2023-07-18 19:27:51 +0300

    update seeders and factories

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Club;
 use App\Models\ClubSubScription;
 use App\Models\User;
 use App\Models\UserSubscription;
+use Database\Factories\ClubSubscriptionFactory;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -21,7 +22,7 @@ class UserSeeder extends Seeder
     {
         User::factory()->has(
             Club::factory()->count(1)->has(
-                ClubSubScription::factory()->count(2)->has(
+                ClubSubscriptionFactory::factory()->count(2)->has(
                     UserSubscription::factory()->has(User::factory()->count(1)
                         ->state(function (array $attributes, User $user) {
                             return ['type' => 'customer'];
